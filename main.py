b43b095c7f6841e3d049039613928b7d98070e44
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-12-15 09:34:21 +0100

    Implement Data Transfer Object for a customer class
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

---
+++ main.py
@@ -0,0 +1,36 @@
+from typing import Dict, Any
+
+
+class Customer:
+    def __init__(self, customer_id, username) -> None:
+        self.customer_id: int = customer_id
+        self.username: str = username
+
+    def __str__(self) -> str:
+        return f"{self.customer_id} --- {self.username}"
+
+
+class CustomerRepository:
+    def __init__(self) -> None:
+        self.customer_repository: dict = {}
+
+    def set(self, customer: Any) -> Dict[int, str]:
+        set_result = self.customer_repository[customer.customer_id] = customer
+        return set_result
+
+    def get(self, customer_id: int) -> dict:
+        customer = self.customer_repository[customer_id]
+        return customer
+
+    def pop(self, customer_id: int) -> dict:
+        customer = self.customer_repository.pop(customer_id)
+        return customer
+
+    def find(self, username: str) -> list:
+        result = []
+
+        for key, values in self.customer_repository.items():
+            if values.username == username:
+                result.append(self.customer_repository[key])
+
+        return result
